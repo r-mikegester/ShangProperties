cc73ef7e3bf1b685037aa877fb927d526fb14501
Mike Gester <mikegester.sabuga023@gmail.com>
2025-05-08 15:03:05 +0800

    added placeholder pages for later

---
+++ src/pages/Haraya.tsx
@@ -0,0 +1,10 @@
+const Haraya = () => {
+    return (
+        <div className="p-6 text-center">
+            <h1 className="text-3xl font-bold">Haraya</h1>
+        </div>
+        
+    );
+};
+
+export default Haraya;
---
+++ src/pages/WackWack.tsx
@@ -0,0 +1,9 @@
+const WackkWack = () => {
+    return (
+        <div className="p-6 text-center">
+            <h1 className="text-3xl font-bold">Welcome to Shang Properties</h1>
+        </div>
+    );
+};
+
+export default WackkWack;
