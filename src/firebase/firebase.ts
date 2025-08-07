041fb2b381b495065afd9d89d6f18322bfc796fd
Mike Gester <mikegester.sabuga023@gmail.com>
2025-08-08 06:17:34 +0800

    feat: Add Firebase storage initialization to firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAI_8OGCM7Pg-2JQvu02QKKJAsMD-WfEjc",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app); // <-- change this line
+export const storage = getStorage(app); // <-- add this line
