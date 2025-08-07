5440f0b540a99cdd47911daa7632f8cc506c7bd3
Mike Gester <mikegester.sabuga023@gmail.com>
2025-08-08 06:06:25 +0800

    feat: Wrap AppRoutes with NotificationProvider for improved context management

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,23 @@ import { createRoot } from "react-dom/client";
 import AppRoutes from "./router/routes"; // 👈 import your routing setup
 import "./assets/styles/index.css";
 import { Analytics } from "@vercel/analytics/react";
+import { NotificationProvider } from "./context/NotificationContext";
 
 if (import.meta.env.PROD) {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <AppRoutes />
-      <Analytics />
+      <NotificationProvider>
+        <AppRoutes />
+        <Analytics />
+      </NotificationProvider>
     </StrictMode>
   );
 } else {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <AppRoutes />
+      <NotificationProvider>
+        <AppRoutes />
+      </NotificationProvider>
     </StrictMode>
   );
 }
